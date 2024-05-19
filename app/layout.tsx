86f27dc115516ec65222ee3942be1cedc004fb4c
ItsMattG <gleesonmatt96@gmail.com>
2024-05-19 10:08:50 +1000

    Upgraded next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Poppins } from '@next/font/google'
+import { Poppins } from "next/font/google"
 
 // const inter = Inter({ subsets: ["latin"] });
 
