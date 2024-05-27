dc2d80cbe9b562318e92f895e23f8bc252cfd358
ItsMattG <gleesonmatt96@gmail.com>
2024-05-28 05:09:36 +1000

    Added privacy-policy and ToS

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+export default function TermsOfService() {
+	return (
+		<div className="bg-background-primary">
+			<main className="max-w-xl mx-auto text-main-text">
+				<div className="p-5">
+					<a className="btn btn-ghost" href="/">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+							<path fill-rule="evenodd" d="M15 10a.75.75 0 01-.75.75H7.612l2.158 1.96a.75.75 0 11-1.04 1.08l-3.5-3.25a.75.75 0 010-1.08l3.5-3.25a.75.75 0 111.04 1.08L7.612 9.25h6.638A.75.75 0 0115 10z" clip-rule="evenodd"></path>
+						</svg>
+						Back
+					</a>
+					<h1 className="text-3xl font-extrabold pb-6">Privacy Policy for WatchWise</h1>
+					<p className="leading-relaxed whitespace-pre-wrap" style={{ fontFamily: 'sans-serif' }}>
+						Last Updated: 27-05-2024
+						<br/>
+						<br/>
+						Thank you for visiting WatchWise ("we," "us," or "our"). This Privacy Policy outlines how we collect, use, and protect your personal and non-personal information when you use our website located at [TO DO: Add website] (the "Website").
+						<br/>
+						<br/>
+						By accessing or using the Website, you agree to the terms of this Privacy Policy. If you do not agree with the practices described in this policy, please do not use the Website.
+						<br/>
+						<br/>
+						<strong>1. Information We Collect</strong>
+						<br/>
+						<strong>1.1 Personal Data</strong>
+						<br/>
+						<br/>
+						We collect the following personal information from you:
+						<br/>
+						<br/>
+					</p>
+						<ul className="leading-relaxed whitespace-pre-wrap" style={{ listStyleType: 'disc', paddingLeft: '16px', fontFamily: 'sans-serif' }}>
+							<li><strong>Name:</strong> We collect your name to personalise your experience and communicate with you effectively.</li>
+							<li><strong>Email:</strong> We collect your email address to send you important information regarding your orders, updates, and communication.</li>
+							<li><strong>Payment Information:</strong> We collect payment details to process your orders securely. However, we do not store your payment information on our servers. Payments are processed by trusted third-party payment processors.</li>
+						</ul>
+					<p className="leading-relaxed whitespace-pre-wrap" style={{ fontFamily: 'sans-serif' }}>
+						<br/>
+						<strong>1.2 Non-Personal Data</strong>
+						<br/>
+						<br/>
+						We may use web cookies and similar technologies to collect non-personal information such as your IP address, browser type, device information, and browsing patterns. This information helps us to enhance your browsing experience, analyse trends, and improve our services.
+						<br/>
+						<br/>
+						<strong>2. Purpose of Data Collection</strong>
+						<br/>
+						<br/>
+						We collect and use your personal data for the sole purpose of order processing. This includes:
+						<br/>
+						<br/>
+					</p>
+						<ul className="leading-relaxed whitespace-pre-wrap" style={{ listStyleType: 'disc', paddingLeft: '16px', fontFamily: 'sans-serif' }}>
+							<li>Processing your orders</li>
+							<li>Sending order confirmations</li>
+							<li>Providing customer support</li>
+							<li>Keeping you updated about the status of your orders</li>
+						</ul>
+					<p className="leading-relaxed whitespace-pre-wrap" style={{ fontFamily: 'sans-serif' }}>
+						<br/>
+						<strong>3. Data Sharing</strong>
+						<br/>
+						<br/>
+						We do not share your personal data with any third parties except as required for order processing (e.g., sharing your information with payment processors). We do not sell, trade, or rent your personal information to others.
+						<br/>
+						<br/>
+						<strong>4. Children's Privacy</strong>
+						<br/>
+						<br/>
+						WatchWise is not intended for children under the age of 13. We do not knowingly collect personal information from children. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us at the email address provided below.
+						<br/>
+						<br/>
+						<strong>5. Updates to the Privacy Policy</strong>
+						<br/>
+						<br/>
+						We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any updates will be posted on this page, and we may notify you via email about significant changes.
+						<br/>
+						<br/>
+						<strong>6. Contact Information</strong>
+						<br/>
+						<br/>
+						If you have any questions, concerns, or requests related to this Privacy Policy, you can contact us at:
+						<br/>
+						<br/>
+						Email: [TO DO: Add email address]
+						<br/>
+						<br/>
+						By using WatchWise, you consent to the terms of this Privacy Policy.
+					</p>
+				</div>
+			</main>
+		</div>
+	);
+}
---
+++ app/tos/page.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+export default function TermsOfService() {
+	return (
+		<div className="bg-background-primary">
+			<main className="max-w-xl mx-auto text-main-text">
+				<div className="p-5">
+					<a className="btn btn-ghost" href="/">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+							<path fill-rule="evenodd" d="M15 10a.75.75 0 01-.75.75H7.612l2.158 1.96a.75.75 0 11-1.04 1.08l-3.5-3.25a.75.75 0 010-1.08l3.5-3.25a.75.75 0 111.04 1.08L7.612 9.25h6.638A.75.75 0 0115 10z" clip-rule="evenodd"></path>
+						</svg>
+						Back
+					</a>
+					<h1 className="text-3xl font-extrabold pb-6 text-nowrap">Terms and Conditions for WatchWise</h1>
+					<p className="leading-relaxed whitespace-pre-wrap" style={{ fontFamily: 'sans-serif' }}>
+						Last Updated: 27-05-2024
+						<br/>
+						<br/>
+						Welcome to WatchWise!
+						<br/>
+						<br/>
+						These Terms of Service ("Terms") govern your use of the WatchWise website at [TO DO: Add Website] (the "Website") and the services provided by WatchWise. By using our Website and services, you agree to these Terms.
+						<br/>
+						<br/>
+						<strong>1. Description of WatchWise</strong>
+						<br/>
+						<br/>
+						WatchWise is a platform that offers tools to analyse streaming data and provide insights to users. Our services include providing code packages for creating applications that analyse streaming data, along with customer support to assist you with using our services.
+						<br/>
+						<br/>
+						<strong>2. Ownership and Usage Rights</strong>
+						<br/>
+						<br/>
+						When you purchase a package from WatchWise, you gain the right to use the tool and code packages for your personal or commercial projects. You may not resell or redistribute the code packages or tool without our permission. We offer a full refund within 7 days of purchase if you are not satisfied with our services.
+						<br/>
+						<br/>
+						<strong>3. User Data and Privacy</strong>
+						<br/>
+						<br/>
+						We collect and store user data, including name, email, and payment information, as necessary to provide our services. For details on how we handle your data, please refer to our [Privacy Policy]([TO DO: Add Website]).
+						<br/>
+						<br/>
+						<strong>4. Non-Personal Data Collection</strong>
+						<br/>
+						<br/>
+						We use web cookies to collect non-personal data for the purpose of improving our services and user experience.
+						<br/>
+						<br/>
+						<strong>5. Governing Law</strong>
+						<br/>
+						<br/>
+						These Terms are governed by the laws of Australia.
+						<br/>
+						<br/>
+						<strong>6. Updates to the Terms</strong>
+						<br/>
+						<br/>
+						We may update these Terms from time to time. Users will be notified of any changes via email or through notifications on our Website.
+						<br/>
+						<br/>
+						For any questions or concerns regarding these Terms of Service, please contact us at [TO DO: Add email address].
+						<br/>
+						<br/>
+						Thank you for using WatchWise!
+					</p>
+				</div>
+			</main>
+		</div>
+	);
+}
